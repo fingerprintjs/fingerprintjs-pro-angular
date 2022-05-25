bede3c9121f366778ce1442404c9c4cb9cd7fd2e
Ilya Taratukhin <darkilfa@gmail.com>
2022-05-25 16:40:36 +0300

    feat: create ng-fingerprintjs-pro library

---
+++ projects/ng-fingerprintjs-pro/src/lib/interfaces/i-fingerprintjs-pro-settings.ts
@@ -0,0 +1,5 @@
+import { FpjsClientOptions } from "@fingerprintjs/fingerprintjs-pro-spa";
+
+export interface IFingerprintjsProSettings {
+  clientOptions: FpjsClientOptions ;
+}
--- projects/ng-fingerprintjs-pro/src/lib/ng-fingerprintjs-pro.service.ts
+++ projects/ng-fingerprintjs-pro/src/lib/ng-fingerprintjs-pro.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { FpjsClient, GetOptions } from '@fingerprintjs/fingerprintjs-pro-spa';
+import { NG_FINGERPTINTJS_PRO_SETTINGS_TOKEN } from './tokens/ng-fingerprintjs-pro-settings-token';
+import { IFingerprintjsProSettings } from './interfaces/i-fingerprintjs-pro-settings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NgFingerprintjsProService {
-
-  constructor() { }
+  private fingerprintJsAgent: FpjsClient
+  constructor(
+    @Inject(NG_FINGERPTINTJS_PRO_SETTINGS_TOKEN) private readonly settings: IFingerprintjsProSettings
+  ) {
+    console.log('NgFingerprintjsProService constructor', settings);
+    this.fingerprintJsAgent = new FpjsClient(settings.clientOptions)
+    this.fingerprintJsAgent.init();
+  }
+  getVisitorData(options?: GetOptions<boolean>) {
+    return this.fingerprintJsAgent.getVisitorData(options);
+  }
+  clearCache() {
+    return this.fingerprintJsAgent.clearCache();
+  }
 }
