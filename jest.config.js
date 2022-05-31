bfe5819522d965924ffe3b9c7782fa4a9be9a578
Ilya Taratukhin <darkilfa@gmail.com>
2022-05-31 16:30:23 +0700

    chore: add eslint and prettier

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'jest-preset-angular',
-  roots: [
-    'projects/fingerprintjs-pro-angular/src/'
-  ],
+  roots: ['projects/fingerprintjs-pro-angular/src/'],
   setupFilesAfterEnv: ['./test.ts'],
-  testRegex: '.+spec.ts$'
-}
+  testRegex: '.+spec.ts$',
+};
