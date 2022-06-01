cf9d4fd50474f709cedc5922e806e07b8aa6235f
Ilya Taratukhin <darkilfa@gmail.com>
2022-06-01 12:08:16 +0700

    docs: added comment about generate-version script purpose

--- bin/generate-version.js
+++ bin/generate-version.js
@@ -1,3 +1,9 @@
+/**
+ * Angular builder uses an src folder as rootDir for the code
+ * So if we will try to import package.json that is situated outside the src folder, we will get an error
+ * Similar problem is described here https://stackoverflow.com/questions/51850063/rootdir-is-expected-to-contain-all-source-files
+ */
+
 const packageInfo = require('../projects/fingerprintjs-pro-angular/package.json');
 const { writeFileSync } = require('fs');
 
