793b2d03f8a15e3ad3b261b419568865131e560c
Ilya Taratukhin <darkilfa@gmail.com>
2022-11-10 19:28:41 +0100

    chore: use current version in `generate-version` if it wasn't provided

--- bin/generate-version.js
+++ bin/generate-version.js
@@ -8,14 +8,11 @@ const { writeFileSync, readFileSync } = require('fs');
 const packageFilePath = './projects/fingerprintjs-pro-angular/package.json';
 const versionFilePath =
   './projects/fingerprintjs-pro-angular/src/lib/version.ts';
-const newVersion = process.argv[2];
-
-if (!newVersion) {
-  console.error('You should provide new version number');
-  process.exit(1);
-}
 
 const packageInfo = JSON.parse(readFileSync(packageFilePath).toString());
+
+const newVersion = process.argv[2] || packageInfo.version;
+
 packageInfo.version = newVersion;
 writeFileSync(packageFilePath, JSON.stringify(packageInfo, null, '  '));
 
