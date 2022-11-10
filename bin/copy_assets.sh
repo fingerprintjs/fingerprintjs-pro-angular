1fb99e8908fbe30be83ce9b3c0aef6a498dca425
Ilya Taratukhin <darkilfa@gmail.com>
2022-11-10 19:19:16 +0100

    chore: use semantic-release for publishing

---
+++ bin/copy_assets.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp LICENSE dist/fingerprintjs-pro-angular
+cp README.md dist/fingerprintjs-pro-angular
+cp CHANGELOG.md dist/fingerprintjs-pro-angular
--- bin/generate-version.js
+++ bin/generate-version.js
@@ -3,14 +3,24 @@
  * So if we will try to import package.json that is situated outside the src folder, we will get an error
  * Similar problem is described here https://stackoverflow.com/questions/51850063/rootdir-is-expected-to-contain-all-source-files
  */
+const { writeFileSync, readFileSync } = require('fs');
 
-const packageInfo = require('../projects/fingerprintjs-pro-angular/package.json');
-const { writeFileSync } = require('fs');
+const packageFilePath = './projects/fingerprintjs-pro-angular/package.json';
+const versionFilePath =
+  './projects/fingerprintjs-pro-angular/src/lib/version.ts';
+const newVersion = process.argv[2];
 
-const filePath = './projects/fingerprintjs-pro-angular/src/lib/version.ts';
+if (!newVersion) {
+  console.error('You should provide new version number');
+  process.exit(1);
+}
+
+const packageInfo = JSON.parse(readFileSync(packageFilePath).toString());
+packageInfo.version = newVersion;
+writeFileSync(packageFilePath, JSON.stringify(packageInfo, null, '  '));
 
 const versionContent = `export const packageVersion = '${packageInfo.version}';\n`;
 
-writeFileSync(filePath, versionContent);
+writeFileSync(versionFilePath, versionContent);
 
 console.log('Version generated successfully');
