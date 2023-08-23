6e57218b9b3db857d03841c17efc9b1eaa9bd0fa
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-08-23 14:20:47 +0200

    ci: migrate to shared workflows

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,10 @@ module.exports = {
   roots: ['projects/fingerprintjs-pro-angular/src/'],
   setupFilesAfterEnv: ['./test.ts'],
   testRegex: '.+spec.ts$',
+  collectCoverageFrom: ['./src/**/**.{ts,tsx}'],
+  coverageReporters: [
+    'lcov',
+    'json-summary',
+    ['text', { file: 'coverage.txt', path: './' }],
+  ],
 };
