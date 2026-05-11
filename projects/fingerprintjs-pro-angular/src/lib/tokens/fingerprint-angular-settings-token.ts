9c8a2d5f6f0bee611844bf937a165cd7caa5ea45
Orkun <orkuncakilkaya@gmail.com>
2026-05-11 15:03:59 +0300

    refactor: update token name for settings

--- projects/fingerprintjs-pro-angular/src/lib/tokens/fingerprint-angular-settings-token.ts
+++ projects/fingerprintjs-pro-angular/src/lib/tokens/fingerprint-angular-settings-token.ts
@@ -2,7 +2,7 @@ import { InjectionToken } from '@angular/core'
 import { FingerprintSettings } from '../interfaces/fingerprint-settings'
 
 export const FINGERPRINT_ANGULAR_SETTINGS_TOKEN = new InjectionToken<FingerprintSettings>(
-  'ng-fingerprintjspro-settings-token',
+  'ng-fingerprint-settings-token',
   {
     factory: () => ({ startOptions: { apiKey: '' } }),
   }
