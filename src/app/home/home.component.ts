1440dd637b548450a5a745c87d805cc3599a1fc5
Orkun <orkuncakilkaya@gmail.com>
2026-05-11 15:22:11 +0300

    refactor: remove usage of 'extended' from results on demo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,23 +10,21 @@ export class HomeComponent {
   constructor(private fingerprintAngularService: FingerprintAngularService) {}
 
   eventId = 'Press "Identify" button to get eventId'
-  extendedResult: Fingerprint.GetResult | null = null
+  result: Fingerprint.GetResult | null = null
 
-  get extendedResultJSON() {
-    return JSON.stringify(this.extendedResult, null, 2)
+  get resultJSON() {
+    return JSON.stringify(this.result, null, 2)
   }
 
   async onButtonClick(): Promise<void> {
     try {
-      const data = await this.fingerprintAngularService.getVisitorData({
-        extendedResult: true,
-      } as any)
+      const data = await this.fingerprintAngularService.getVisitorData()
       this.eventId = data.event_id
-      this.extendedResult = data
+      this.result = data
     } catch (error) {
       if (error instanceof Error) {
         this.eventId = `${error.name}: ${error.message}`
-        this.extendedResult = null
+        this.result = null
       }
     }
   }
@@ -34,6 +32,6 @@ export class HomeComponent {
   onClearCacheClick() {
     this.fingerprintAngularService.clearCache()
     this.eventId = 'Press button to get eventId again'
-    this.extendedResult = null
+    this.result = null
   }
 }
